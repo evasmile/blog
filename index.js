2ce53fb6fbfd7b034b37a762954af978885354c4
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-04-16 20:26:44 +0200

    add

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import jsdom from "jsdom";
+import jQuery from 'jquery';
+
+import { log } from 'console';
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -9,7 +13,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.static('public'))
 
+const dom = new jsdom.JSDOM("")
+const $ = jQuery(dom.window)
+
 app.get('/',function(req,res){
+   
     res.render('home.ejs',{blog:blogs})
 })
 
@@ -18,9 +26,70 @@ app.get('/newBlog.ejs',function(req,res){
 })
 
 app.post('/newBlog',function(req,res){
+   
+    addBlog( {id:blogs.length+1,
+        blog:req.body}  );
 
-    addBlog(req.body);
     res.redirect('/')
+
+   
+})
+
+app.get('/moreread/:id',function(req,res){
+  
+   blogs.forEach(blog => {
+    
+    if(blog.id.toString()===req.params.id){
+        
+       res.render('blog.ejs',{blog:blog})
+       
+        
+    }
+   });
+  
+})
+
+app.get('/edit/:id',function(req,res){
+    blogs.forEach(blog => {
+    if(blog.id.toString()===req.params.id){  
+           
+            res.render('editBlog.ejs',{blog:blog})
+         return
+        
+    }
+});
+
+app.post('/blog/edit/:id',function(req,res){
+
+    blogs.forEach(blog => {
+    
+        
+        if(blog.id.toString()===req.params.id){
+
+            
+            blog.blog.author = req.body.author;
+            blog.blog.title = req.body.title;
+            blog.blog.message = req.body.message;
+         
+            res.render('blog.ejs',{blog:blog})
+            return
+        }
+       });
+})
+
+
+
+})
+
+app.get('/delete/:id',function(req,res){
+    for (let index = 0; index < blogs.length; index++) {
+        if(blogs[index].id.toString()===req.params.id){
+           blogs= blogs.slice(index)
+            res.redirect('/')
+            return
+        }
+        
+    }
 })
 
 app.listen(port,function(){
@@ -28,7 +97,7 @@ app.listen(port,function(){
 })
 
 
-const blogs=[];
+let blogs=[];
 function addBlog(blog){
  blogs.push(blog)
 }
